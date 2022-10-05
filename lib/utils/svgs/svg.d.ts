6b480e4691deb6eb901901c0ed86bf7843a9ee26
Ha Thai Hoang <jonlefcun@gmail.com>
2022-10-05 10:59:01 +0700

    commit 4

--- lib/utils/svgs/svg.d.ts
+++ lib/utils/svgs/svg.d.ts
@@ -1,3 +1,2 @@
-import React from "react";
-declare const loadSVG: (fileName: string, defaultIcon?: string) => React.LazyExoticComponent<React.ComponentType<any>>;
-export { loadSVG, };
+declare const loadSVG: (fileName: string, defaultIcon?: string) => (props: any) => JSX.Element;
+export { loadSVG };
